623257b312f7322125e087cc8a1dc6e24c9efe8b
Parvej khan <83569207+dashingshree6@users.noreply.github.com>
2022-12-08 17:43:15 +0530

    Create VendorOrderTracking.js

---
+++ frontend/screens/Vendor/LiveOrderTracking/VendorOrderTracking.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function VendorOrderTracking() {
+  return (
+    <View>
+      <Text>VendorOrderTracking</Text>
+    </View>
+  )
+}
