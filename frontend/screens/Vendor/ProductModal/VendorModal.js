fda2664d79c28a1599d0fccbe4bfb19b85ee1138
Parvej khan <83569207+dashingshree6@users.noreply.github.com>
2022-12-08 17:43:06 +0530

    Create VendorModal.js

---
+++ frontend/screens/Vendor/ProductModal/VendorModal.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function VendorModal() {
+  return (
+    <View>
+      <Text>VendorModal</Text>
+    </View>
+  )
+}
