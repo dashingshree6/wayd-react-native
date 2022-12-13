4128e8b4b9da937b1cc5904022e3b68318779a62
Parvej khan <83569207+dashingshree6@users.noreply.github.com>
2022-12-13 16:38:32 +0530

    Update Login.js

--- frontend/screens/Login/Login.js
+++ frontend/screens/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import {
     SafeAreaView,
     ScrollView,
@@ -15,8 +15,100 @@ import {
     Button 
     } 
      from '@rneui/themed';
-  
+import { signin, authenticate, isAuthenticated } from '.';
+import SyncStorage from 'sync-storage';
+
 const Login = ({ navigation  }) => {
+    const [values, setValues] = useState({
+        phone_number: "",
+        password: "",
+        error: "",
+        loading: false,
+        didRedirect: false,
+      });
+
+    const { phone_number, password, error, loading, didRedirect } = values;
+    const { user } = isAuthenticated();
+
+    const handleChange = (name) => (event) => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+    }; 
+
+    const onSubmit = () => {
+        setValues({ ...values, error: false, loading: true });
+        signin({ phone_number, password })
+          .then((data) => {
+            console.log(data.data);
+    
+            if (data.error) {
+              setValues({ ...values, error: data.data.error, loading: false });
+            } else {
+              if (data.data.user.role === 0) {
+                setValues({
+                  ...values,
+                  error: "not authorized",
+                  didRedirect: false,
+                });
+              } else {
+                authenticate(data.data, () => {
+                  setValues({
+                    ...values,
+                    didRedirect: true,
+                  });
+                });
+                //new added
+                SyncStorage.set("role",data.data.user.role)
+                console.log(SyncStorage.get("role"))
+                if (data.data.token) {
+                    if (data.data.user && data.data.user.role === 1) {
+                      // return <Redirect to="/admin/dashboard" />;
+                      return navigation.navigate("SalesHomepage")
+                    } else {
+                      return navigation.navigate("VendorHomepage")
+                    }
+                  }
+                  if (isAuthenticated() && user.role === 1) {
+                    return navigation.navigate("SalesHomepage")
+                  }
+                  if (isAuthenticated() && user.role === 0) {
+                      return navigation.navigate("VendorHomepage")
+                  }
+
+              }
+              //
+            }
+          })
+          .catch((err) =>
+            setValues({
+              ...values,
+              error: err.response.data.error,
+              loading: false,
+            })
+          );
+      };
+
+    const performRedirect = () => {
+        //TODO: do a redirect here
+        if (didRedirect) {
+          if (user && user.role === 1) {
+            // return <Redirect to="/admin/dashboard" />;
+            return navigation.navigate("SalesHomepage")
+          } else {
+            return navigation.navigate("VendorHomepage")
+          }
+        }
+        if (isAuthenticated() && user.role === 1) {
+          return navigation.navigate("SalesHomepage")
+        }
+        if (isAuthenticated() && user.role === 0) {
+            return navigation.navigate("VendorHomepage")
+        }
+      };
+    
+    //   useEffect(()=>{
+    //     SyncStorage.remove('jwt')
+    //   })
+
     return (
         <SafeAreaView>
             <ScrollView>
@@ -35,6 +127,7 @@ const Login = ({ navigation  }) => {
                     //     color='black'
                     //     />
                     //     }
+                    onChangeText={value => setValues({ ...values, error: false, phone_number: value })}
                     />
                     <Text style={styles.password}>Password</Text>
                     <Input
@@ -49,12 +142,14 @@ const Login = ({ navigation  }) => {
                     //     color='black'
                     //     />
                     //     }
+                    onChangeText={value => setValues({ ...values, error: false, password: value })}
                     />
                     <Text style={styles.forgot_password}>Forgot Password ?</Text>
                     <View style={styles.btn_pw}> 
                         <Button
                         title="Login"
-                        // onPress={() => navigation.navigate("SalesHomepage")}
+                        disabled={phone_number && password ? false : true}
+                        onPress={() => onSubmit()}
                         buttonStyle={{ 
                             backgroundColor: 'rgba(127, 220, 103, 1)', 
                         }}
