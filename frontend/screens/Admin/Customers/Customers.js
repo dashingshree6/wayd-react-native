f395865372c098c8eb2106ea3ce5b8c95f3111be
AMRUT SAGAR V <amrutsagar06@gmail.com>
2022-12-24 01:25:08 +0430

    cash collection ui updated

--- frontend/screens/Admin/Customers/Customers.js
+++ frontend/screens/Admin/Customers/Customers.js
@@ -6,7 +6,6 @@ import {
   StyleSheet,
   Text,
   View,
-  Button,
   Alert,
   Modal,
   Pressable,
@@ -15,11 +14,11 @@ import {
 import {API, TOKEN} from '../../backend';
 import CashImg from './cashImg.jpg';
 import modalImg from './modalImg.jpg';
+import { Button } from '@rneui/themed';
 
 export default function Customers() {
   const [allUsers, setAllUser] = useState();
   const [apiTrigger, setApiTrigger] = useState();
-
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedUser, setSelectedUser] = useState({
     Amount: 0,
@@ -27,9 +26,6 @@ export default function Customers() {
     isCashCollected: true,
     userId: '',
   });
-
-  // const TOKEN =
-  //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDA3ZjRmM2VmOTRjMTAwMjQ4ODI1N2QiLCJpYXQiOjE2NzA3MTgxMDF9.ITnJjF8atFSnGl7dwBejIVLRnPantE5F8YWsW1uehHY';
 
   React.useEffect(() => {
     let url = `${API}/users`;
@@ -62,10 +58,10 @@ export default function Customers() {
     <>
       <ScrollView contentContainerStyle={styles.contentContainer}>
         <View style={styles.imgView}>
-          <Image style={styles.cashImg} source={CashImg} />
+          {/* <Image style={styles.cashImg} source={CashImg} /> */}
         </View>
         <View style={styles.allUsersView}>
-          <Text style={styles.dashboardTitle}>Cash Collection Dashbaord</Text>
+          <Text style={styles.dashboardTitle}>Cash Collection</Text>
           {allUsers
             ?.filter((item, index) => item.role === 0)
             .map((item, key) => (
@@ -90,7 +86,7 @@ export default function Customers() {
                         setSelectedUser({
                           userId: `${item._id}`,
                           Amount: item.due_amount,
-                          time: '19:30 ',
+                          time: '19:30',
                           isCashCollected: true,
                         });
                         setModalVisible(true);
@@ -100,20 +96,23 @@ export default function Customers() {
                   </View>
                 ) : (
                   <View>
-                    <Text>Role: {item.role}</Text>
-                    <Text>Name: {item.username}</Text>
-                    <Text>User id: {item._id}</Text>
-                    <Text>
-                      Max Limit: {item.max_due ? item.max_due : `Not Assigned`}
-                    </Text>
-
-                    <View>
-                      <Text>Due: {item.due_amount}</Text>
-                    </View>
-
-                    <Button
+                    {/* <Text>Role: {item.role}</Text> */}
+              <View style={styles.cash_head}>
+              <Text style={styles.cash_head_2}>{item.username}</Text>
+              <Text style={styles.due_max_limit}>Due <Text style={styles._ui_username}>{item.due_amount}</Text></Text>
+                    {/* <Text>User id: {item._id}</Text> */}
+              <Text style={styles.due_max_limit}>Max Limit: {item.max_due ? item.max_due : `Not Assigned`}</Text></View>
+                      <Button
                       title="Update Due Amount"
-                      color="black"
+                      color="#0F4C75"
+                      
+                      containerStyle={{
+                        width: 190,
+                        marginHorizontal: 90,
+                        marginVertical: 10,  
+                        borderRadius:10
+                      }}
+                     
                       onPress={() => {
                         setSelectedUser({
                           userId: `${item._id}`,
@@ -125,6 +124,7 @@ export default function Customers() {
                       }}
                       id={item._id}
                     />
+                   
                   </View>
                 )}
               </View>
@@ -165,7 +165,7 @@ export default function Customers() {
                   onPress={() => {
                     handlepost();
                   }}
-                  color="black"
+                  
                 />
               </View>
               <View style={{width: 100, height: 50}}>
@@ -174,6 +174,7 @@ export default function Customers() {
                   onPress={() => {
                     setModalVisible(!modalVisible);
                   }}
+                  color="black"
                 />
               </View>
             </View>
@@ -187,15 +188,28 @@ export default function Customers() {
 const styles = StyleSheet.create({
   usersCard: {
     width: '90%',
-    margin: 10,
-    backgroundColor: '#189AB4',
-    borderRadius: 10,
+    margin: 20,
+    backgroundColor: '#fff',
+    borderRadius: 20,
     shadowColor: 'black',
     textAlign: 'center',
-    paddingTop: 5,
-    paddingBottom: 5,
+    paddingTop: 23,
+    height:170,
     paddingLeft: 10,
     paddingRight: 10,
+    elevation:10,
+    shadowColor:'black'
+  },
+  _ui_username:{
+      color:"green",
+      },
+  cash_head:{
+marginLeft:20
+  },
+  cash_head_2:{
+fontWeight:'bold',
+fontSize:19,
+color:'black',
   },
   postCancelView: {
     flex: 1,
@@ -205,7 +219,11 @@ const styles = StyleSheet.create({
     width: '95%',
     height: 100,
   },
-
+  due_max_limit:{
+    color:'black',
+    fontSize:14,
+    padding:2
+  },
   cashImg: {
     width: 400,
     height: 200,
@@ -231,6 +249,12 @@ const styles = StyleSheet.create({
     padding: 8,
     marginTop: 10,
   },
+  // update_due_btn:{
+  //   borderRadius:20,
+  //   color:'red'
+    
+
+  // },
   centeredView: {
     flex: 1,
     justifyContent: 'center',
