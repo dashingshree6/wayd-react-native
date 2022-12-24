36fdccb59d28de77812885c6fe517be8a1362d1b
Santosh Kumar <PLEASEFORWARDYOURMAIL@GMAIL.COM>
2022-12-24 13:57:42 +0530

    Merging the Suppliers Page

--- frontend/screens/backend.js
+++ frontend/screens/backend.js
@@ -1,4 +1,4 @@
-export const API = 'https://ed48-49-205-239-58.in.ngrok.io/api';
+export const API = ' https://2a4a-49-205-239-58.in.ngrok.io/api';
 
 export const TOKEN =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDA3ZjRmM2VmOTRjMTAwMjQ4ODI1N2QiLCJpYXQiOjE2NzE4MTg2MDB9.QqYBuZs4YIbqsOOUsR1rD61mMNP0Bgxjo4-cc0P0H5U';
